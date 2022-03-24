f33239b69d47853f60c9f2fa99a223b0e342ed46
Dorian Matić <dorian.matic@ingemark.com>
2022-03-24 20:29:27 +0100

    added bootstrap and bootstrap icons

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,24 @@ ActiveRecord::Schema.define(version: 2022_03_21_143716) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "followers", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "subscriber_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subscriber_id"], name: "index_followers_on_subscriber_id"
+    t.index ["user_id"], name: "index_followers_on_user_id"
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "subscribed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subscribed_id"], name: "index_followings_on_subscribed_id"
+    t.index ["user_id"], name: "index_followings_on_user_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "likeable_id", null: false
@@ -97,6 +115,10 @@ ActiveRecord::Schema.define(version: 2022_03_21_143716) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "followers", "users"
+  add_foreign_key "followers", "users", column: "subscriber_id"
+  add_foreign_key "followings", "users"
+  add_foreign_key "followings", "users", column: "subscribed_id"
   add_foreign_key "likes", "users"
   add_foreign_key "relations", "users", column: "followee_id"
   add_foreign_key "relations", "users", column: "follower_id"
