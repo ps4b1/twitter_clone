c68e04399a6366555e4639dba6eff17b9e22d043
PetarSabolek <psabolek1705@gmail.com>
2022-06-03 11:41:34 +0200

    Cleaning chatroom_user

--- app/controllers/chatroom_users_controller.rb
+++ app/controllers/chatroom_users_controller.rb
@@ -6,14 +6,14 @@ class ChatroomUsersController < ApplicationController
   def destroy
     current_chatroom = Chatroom.find(params[:chatroom])
     chatroom_user = ChatroomUser.find_by(chatroom_id: params[:chatroom], user_id: params[:user])
+    if chatroom_user.admin && current_chatroom.users.count.positive?
+      next_user = current_chatroom.chatroom_users.first
+      next_user.update(admin: true)
+    end
     admin = chatroom_user.admin
     chatroom_user.destroy
     redirect_to chatrooms_path if params[:user] == current_user.id.to_s
     redirect_to edit_chatroom_path(current_chatroom) if !admin && current_chatroom.users.count.positive?
-    if admin && current_chatroom.users.count.positive?
-      next_user = current_chatroom.chatroom_users.first
-      next_user.update(admin: true)
-    end
     current_chatroom.destroy if current_chatroom.users.count.zero?
   end
 end
